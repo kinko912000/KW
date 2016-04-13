6fb22637b3ca3cc0122a2f476fcf13862446ba02
YoshiakiIto <kinko912000@gmail.com>
2016-04-13 15:54:16 +0900

    name 追加

--- db/migrate/20160413065156_create_kw_words.rb
+++ db/migrate/20160413065156_create_kw_words.rb
@@ -2,6 +2,7 @@ class CreateKwWords < ActiveRecord::Migration
   def change
     create_table :kw_words do |t|
 
+      t.string :name, null: false, index: true
       t.timestamps null: false
     end
   end
