95768945a22613c37d232299bf269d73a15f041d
YoshiakiIto <kinko912000@gmail.com>
2016-04-13 16:34:29 +0900

    画面上でURLを入力すると単語を取得する

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -11,7 +11,7 @@ class WordsController < ApplicationController
     ### TODO: エラー処理
     api_url = 'http://so-zou.jp/web-app/text/morpheme/analyze.php'
 
-    doc = Nokogiri::HTML(open("http://www.kochoran.ne.jp/"))
+    doc = Nokogiri::HTML(open(params[:url]))
     body = doc.xpath('//text()').map { |n| n.content }.join('').gsub(/\p{WSpace}+/,' ')
     res = Net::HTTP.post_form(URI.parse(api_url), {text: body})
 
@@ -20,5 +20,7 @@ class WordsController < ApplicationController
 
     words = (strs - Kw::Word.where(name: strs).pluck(:name)).map { |name| Kw::Word.new(name: name) }
     Kw::Word.import words
+
+    redirect_to words_path
   end
 end
