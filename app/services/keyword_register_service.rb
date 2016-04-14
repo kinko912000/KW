8ca149959953c123a9c9ff0a9b01f71f0000e1fd
YoshiakiIto <kinko912000@gmail.com>
2016-04-14 22:36:26 +0900

    文字数制限に対応

--- app/services/keyword_register_service.rb
+++ app/services/keyword_register_service.rb
@@ -9,15 +9,17 @@ module KeywordRegisterService
   def self.register!(url)
     begin
       doc = Nokogiri::HTML(open(url))
-      body = doc.xpath('//text()').map { |n| n.content }.join('').gsub(/\p{WSpace}+/,' ')
-      res = Net::HTTP.post_form(URI.parse(API_URL), {text: body})
+      body = doc.xpath('//text()').map { |n| n.content }.join(' ').force_encoding('UTF-8').scrub('?').gsub(/\p{WSpace}+/,' ')
+      body.scan(/.{1,10000}/).each do |text|
+        res = Net::HTTP.post_form(URI.parse(API_URL), {text: text})
 
-      strs = JSON.parse(res.body).map { |data| data["baseform"] if data["pos"] == "名詞" }.compact.uniq
-      strs = strs.reject { |a| a =~ /\w+/ }
-      strs = strs.map { |str| "胡蝶蘭 #{str}" }
+        strs = JSON.parse(res.body).map { |data| data["baseform"] if data["pos"] == "名詞" }.compact.uniq
+        strs = strs.reject { |a| a =~ /\w+/ }
+        strs = strs.map { |str| "胡蝶蘭 #{str}" }
 
-      words = (strs - Kw::Word.where(name: strs).pluck(:name)).map { |name| Kw::Word.new(name: name) }
-      Kw::Word.import words
+        words = (strs - Kw::Word.where(name: strs).pluck(:name)).map { |name| Kw::Word.new(name: name) }
+        Kw::Word.import words
+      end
     rescue => e
       puts e
     end
